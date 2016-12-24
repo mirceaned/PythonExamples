5fac2d3139292d33d3d9ce8d85c9937203324f17
Mircea N <mirceaned@yahoo.com>
2016-12-24 11:57:48 -0800

    added unit test

--- json_ex/serialization.py
+++ json_ex/serialization.py
@@ -1,7 +1,9 @@
 
 import json
 
-file = open("sample.json", "w")
-table = {'John': 123, 'Doe': 456, 'Mary': 789, 'Lisa': 123}
+class FileReader:
+    def __init__(self):
+        file = open("sample.json", "w")
+        table = {'John': 123, 'Doe': 456, 'Mary': 789, 'Lisa': 123}
 
-json.dump(table, file)
+        json.dump(table, file)
---
+++ json_ex/test.py
@@ -0,0 +1,16 @@
+
+import unittest
+import json
+import serialization
+
+
+class TestSerialization(unittest.TestCase):
+
+    def test_load(self):
+        reader = serialization.FileReader()
+        file = open("sample.json", "r")
+        dictionary = json.load(file)
+        self.assertTrue(dictionary["Lisa"] == 123)
+
+if __name__ == '__main__':
+    unittest.main()
